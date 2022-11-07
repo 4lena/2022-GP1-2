3bfa2b65e3fe451f1c2d7cf2edef826051a18078
Lena Alsuwailem <111577408+4lena@users.noreply.github.com>
2022-11-07 22:32:54 +0300

    Delete 2_ToxicityInspector_App directory

--- 2_ToxicityInspector_App/Db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("vendor/autoload.php");
-
-$client = new MongoDB\Client(
-    'mongodb://localhost:27017/test?retryWrites=true&w=majority'
-);
-$db = $client->Toxicity_Inspector;
-
-?>
