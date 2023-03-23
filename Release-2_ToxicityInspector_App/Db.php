eb0176f98666a4b3c56d60d9390de17dcce8f12c
Lena Alsuwailem <111577408+4lena@users.noreply.github.com>
2023-03-23 15:36:22 +0300

    Add files via upload

--- Release-2_ToxicityInspector_App/Db.php
+++ Release-2_ToxicityInspector_App/Db.php
@@ -2,8 +2,6 @@
 include("vendor/autoload.php");
 
 $client = new MongoDB\Client(
-    'mongodb://localhost:27017/test?retryWrites=true&w=majority'
+    'mongodb+srv://doadmin:s657o4j1Wry8O39g@db-mongodb-blr1-89764-b2f084a2.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-89764'
 );
 $db = $client->Toxicity_Inspector;
-
-?>
