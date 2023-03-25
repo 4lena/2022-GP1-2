f57080c885f863ff16b95b4c261c98a306e16c45
Lena Alsuwailem <111577408+4lena@users.noreply.github.com>
2023-03-25 21:02:14 +0300

    Update Db.php

--- Release-2_ToxicityInspector_App/Db.php
+++ Release-2_ToxicityInspector_App/Db.php
@@ -2,7 +2,7 @@
 include("vendor/autoload.php");
 
 $client = new MongoDB\Client(
-    'mongodb+srv://doadmin:s657o4j1Wry8O39g@db-mongodb-blr1-89764-b2f084a2.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-89764'
+    'mongodb+srv://doadmin:i7X2T8E5KqW4Z610@db-mongodb-blr1-89764-b2f084a2.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-blr1-89764&tls=true'
 );
 $db = $client->Toxicity_Inspector;
 ?>
